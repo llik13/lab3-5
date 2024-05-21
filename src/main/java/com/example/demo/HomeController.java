4e2fa7d0322d11cc99d64a44c2c7e081fc75f76b
llik13 <murchick2004@gmail.com>
2024-05-21 15:49:42 +0300

    comit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,17 +3,14 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
-    @GetMapping
+
+    @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("pageTitle", "Hello JVM world!");
-        model.addAttribute("languages", List.of("Java", "Kotlin", "Groovy", "Clojure"));
+        model.addAttribute("message", "Hello, Thymeleaf!");
         return "index";
     }
 }
+
